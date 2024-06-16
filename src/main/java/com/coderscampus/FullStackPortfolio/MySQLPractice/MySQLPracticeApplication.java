9213461e64e3b754d7eb5aef76d4c537a9f356e8
Zack Laine <rumtehk_uh@yahoo.com>
2024-06-16 13:16:13 -0400

    Getting MySQLPractice set up

---
+++ src/main/java/com/coderscampus/FullStackPortfolio/MySQLPractice/MySQLPracticeApplication.java
@@ -0,0 +1,11 @@
+package com.coderscampus.FullStackPortfolio.MySQLPractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MySQLPracticeApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MySQLPracticeApplication.class, args);
+    }
+}
