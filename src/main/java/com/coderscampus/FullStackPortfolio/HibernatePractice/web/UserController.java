b839b3f362776fd8abe8071fe0f34726dd1680a8
Zack Laine <rumtehk_uh@yahoo.com>
2024-06-29 09:35:56 -0400

    Find by ID all set up

--- src/main/java/com/coderscampus/FullStackPortfolio/HibernatePractice/web/UserController.java
+++ src/main/java/com/coderscampus/FullStackPortfolio/HibernatePractice/web/UserController.java
@@ -1,9 +1,13 @@
 package com.coderscampus.FullStackPortfolio.HibernatePractice.web;
 
+import com.coderscampus.FullStackPortfolio.HibernatePractice.domain.User;
 import com.coderscampus.FullStackPortfolio.HibernatePractice.service.UserService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import java.util.Arrays;
 
 @Controller
 public class UserController {
@@ -18,6 +22,13 @@ public class UserController {
     public String getAllUsers(ModelMap model) {
         model.addAttribute("users", userService.findAll());
 
+        return "users";
+    }
+
+    @GetMapping("/users/{userId}")
+    public String getById(@PathVariable Long userId, ModelMap model) {
+        User user = userService.findById(userId);
+        model.addAttribute("users", Arrays.asList(user));
         return "users";
     }
 }
