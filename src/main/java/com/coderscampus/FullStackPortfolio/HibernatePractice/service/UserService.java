dc7d898d30419eb7b4aff91f7edd20a5e635f89d
Zack Laine <rumtehk_uh@yahoo.com>
2024-06-28 20:21:14 -0400

    Putting in findById, Controller is next

--- src/main/java/com/coderscampus/FullStackPortfolio/HibernatePractice/service/UserService.java
+++ src/main/java/com/coderscampus/FullStackPortfolio/HibernatePractice/service/UserService.java
@@ -5,6 +5,7 @@ import com.coderscampus.FullStackPortfolio.HibernatePractice.repository.UserRepo
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class UserService {
@@ -20,6 +21,7 @@ public class UserService {
     }
 
     public User findById(Long id) {
-        return userRepo.findById(id).get();
+        Optional<User> userOpt = userRepo.findById(id);
+        return userOpt.orElse(new User());
     }
 }
