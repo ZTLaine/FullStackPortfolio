6339e02e5345770bd98512da534f4c1dc8283c3c
Zack Laine <rumtehk_uh@yahoo.com>
2024-07-14 19:40:30 -0400

    Setting up Javascript practice

---
+++ src/main/java/com/coderscampus/FullStackPortfolio/JavascriptPractice/domain/User.java
@@ -0,0 +1,9 @@
+package com.coderscampus.FullStackPortfolio.JavascriptPractice.domain;
+
+import lombok.Data;
+
+@Data
+public class User {
+    private String username;
+    private String password;
+}
