3275e3a151e5afb0f356ebad3f663507d06bef3b
Zack Laine <rumtehk_uh@yahoo.com>
2024-07-23 20:06:58 -0400

    Switched to promises

--- src/main/resources/static/js/register.js
+++ src/main/resources/static/js/register.js
@@ -7,17 +7,17 @@ usernameTextBox.addEventListener('blur', () => {
         'username': usernameTextBox.value
     }
     fetch('users/exists', {
-        method:'POST',
+        method: 'POST',
         headers: {
-            'Content-Type':'application/json'
+            'Content-Type': 'application/json'
         },
         body: JSON.stringify(user)
     }).then((response => response.json()))
         .then((data) => {
-        if(data === true){
-            alert("This username is already taken!")
-        }
-    })
+            if (data === true) {
+                alert("This username is already taken!")
+            }
+        })
 })
 
 passwordTextBox.addEventListener('blur', () => {
@@ -25,44 +25,76 @@ passwordTextBox.addEventListener('blur', () => {
         'password': passwordTextBox.value
     }
     fetch('users/passwordReqs', {
-        method:'POST',
+        method: 'POST',
         headers: {
-            'Content-Type':'application/json'
+            'Content-Type': 'application/json'
         },
         body: JSON.stringify(user)
     }).then((response => response.json()))
         .then((data) => {
-            if(data === false){
+            if (data === false) {
                 alert("Password must be 6-24 characters long.")
-            }
-            else {
+            } else {
                 // passwordTextBox.color = `rgb(255, 255, 255)`
             }
         })
 })
 
 confirmPasswordTextBox.addEventListener('blur', () => {
-    if(confirmPasswordTextBox.value !== passwordTextBox.value){
+    if (confirmPasswordTextBox.value !== passwordTextBox.value) {
         // alert("Passwords must match!")
-        showErrorAnimation(() => {
-            //animation completed here
-            console.log("In callback function")
+        showErrorAnimation().then((message) => {
+            console.log("Now in the callback function!")
         })
-    }else{
+    } else {
         confirmPasswordTextBox.style.border = ``
     }
 })
 
-function showErrorAnimation(callback){
-    console.log("In in animation function")
-    var i = 255
-    var animation = setInterval(() => {
-        i--
-        confirmPasswordTextBox.style.border = `2px solid rgb(255, ${i}, ${i})`
-        if(i <= 0){
-            clearInterval(animation)
-            console.log("Done with animation")
-            callback()
-        }
-    }, 1)
+function showErrorAnimation() {
+    return new Promise((resolve, reject) => {
+        console.log("In animation function")
+        var i = 255
+        var animation = setInterval(() => {
+            i--
+            confirmPasswordTextBox.style.border = `2px solid rgb(255, ${i}, ${i})`
+            if (i <= 0) {
+                resolve("Done with animation code")
+                clearInterval(animation)
+            }
+        }, 1)
+    })
 }
+
+// function showErrorAnimation(callback) {
+//     console.log("In animation function")
+//     var i = 255
+//     var animation = setInterval(() => {
+//         i--
+//         confirmPasswordTextBox.style.border = `2px solid rgb(255, ${i}, ${i})`
+//         if (i <= 0) {
+//             clearInterval(animation)
+//             console.log("Done with animation")
+//             callback()
+//         }
+//     }, 1)
+// }
+
+// Promise demo
+// var myPromise = new Promise((resolve, reject) => {
+//     let i = 2
+//
+//     if( i === 1){
+//         resolve("Hey i - 1, nice")
+//     }
+//     else{
+//         reject("Well that didn't go quite right.  Fail. i != 1.")
+//     }
+// })
+//
+// myPromise.then((message) => {
+//     console.log(message)
+// }).catch((message => {
+//     console.log(message)
+// }))
+
